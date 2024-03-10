c5694037f7f796e90e09f40b62bc0424e15c0728
polinamln <polina77979.la@gmail.com>
2024-03-11 00:46:49 +0100

    1

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,20 +2,28 @@ import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
 import clsx from "clsx";
 import { BiSolidCameraMovie } from "react-icons/bi";
+import { RiMovie2Fill } from "react-icons/ri";
 
 export default function Navigation() {
   const activLink = ({ isActive }) => {
     return clsx(css.link, isActive && css.active);
   };
   return (
-    <div className={css.list}>
-      <BiSolidCameraMovie className={css.icon} />
-      <NavLink className={activLink} to="/">
-        Home
-      </NavLink>
-      <NavLink className={activLink} to="/movies_page">
-        Movies
-      </NavLink>
+    <div className={css.header}>
+      <h1 className={css.title}>
+        YOUR M<RiMovie2Fill />
+        VIE
+      </h1>
+      <p className={css.slogan}>"Discover Your Favorite Movie"</p>
+      <div className={css.list}>
+        <BiSolidCameraMovie className={css.icon} />
+        <NavLink className={activLink} to="/">
+          HOME
+        </NavLink>
+        <NavLink className={activLink} to="/movies_page">
+          MOVIES
+        </NavLink>
+      </div>
     </div>
   );
 }
